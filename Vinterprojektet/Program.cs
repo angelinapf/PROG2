82ce1aaf75365d659d0fa69281dce3da64c1a625
angelinapf <angelina.palaciofrick@elev.ga.ntig.se>
2023-12-04 10:27:51 +0100

    commit and push ex

--- Vinterprojektet/Program.cs
+++ Vinterprojektet/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace Vinterprojektet;
+﻿/* Idéer:
+tamagochi
+film digital library
+music player
+note taking application */
+
+namespace Vinterprojektet;
 class Program
 {
     static void Main(string[] args)
