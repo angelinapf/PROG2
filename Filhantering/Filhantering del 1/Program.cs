26b8e64aa21fc24c52246641946eb95cca701acd
angelinapf <angelina.palaciofrick@elev.ga.ntig.se>
2024-03-12 13:56:59 +0100

    hhh

--- Filhantering/Filhantering del 1/Program.cs
+++ Filhantering/Filhantering del 1/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System.IO;
 
-namespace Filhantering_del_1;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+public class Filhantering{
+    
 }
---
+++ Filhantering/Filhantering del 1/lektion.cs
@@ -0,0 +1,18 @@
+/* using System.Runtime.CompilerServices;
+
+package object2files;
+
+public class MyObject{
+    private string name;
+    private int age;
+    private string occupation;
+
+    public MyObject(string name, int age, string occupation){
+        this.name = name;
+        this.age = age;
+        occupation=occ;
+    }
+
+    public void printData
+}
+*/
